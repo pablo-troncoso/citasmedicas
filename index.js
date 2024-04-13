4c1f9bad733c61e1c375b534e7333a234b8f573a
pablotroncoso <pablotroncoso@gmail.com>
2024-04-13 08:01:15 -0400

    Actualizacion Index.js

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 // Importar Express
 const express = require('express');
+const axios = require('axios'); // Importar Axios
 
-// Crear una instancia de Express
+// Express
 const app = express();
 
-// Definir el puerto y el callback de inicialización
+// Middleware JSON
+app.use(express.json());
+
+// Ruta para el Registro de usuarios
+app.post('/registro', async (req, res) => {
+  try {
+    // Solicitud a la API Random User
+    const response = await axios.get('https://randomuser.me/api/');
+    
+    // Extraer los datos del usuario de la respuesta
+    const usuario = response.data.results[0];
+    
+    // Datos del Usuario
+    console.log(usuario);
+    
+    // Enviar una respuesta al cliente
+    res.status(201).json({ mensaje: 'Registrado exitosamente', usuario });
+  } catch (error) {
+    console.error('Error al Registrar usuario:', error);
+    res.status(500).json({ error: 'Error al registrar usuario' });
+  }
+});
+
+// Definir Puerto 
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor inicializado en el puerto ${PORT}`);
